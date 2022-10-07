b7fd34df276587357940a2d949ba08abfd4a83db
muraoka <ym.animator@gmail.com>
2022-10-07 16:27:34 +0900

    アイテム数を格納したヘッダー情報のアクセス許可を追加

--- src/main/java/com/example/app/controller/ItemApiController.java
+++ src/main/java/com/example/app/controller/ItemApiController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import com.example.app.domain.Item;
 import com.example.app.domain.Place;
 import com.example.app.service.ItemService;
 
+@CrossOrigin("http://localhost:3000")
 @RestController
 @RequestMapping("/api")
 public class ItemApiController {
@@ -36,7 +38,8 @@ public class ItemApiController {
 			@RequestParam(name = "_limit", defaultValue = "5") Integer limit
 			) {
 		var headers = new HttpHeaders();
-		headers.add("X-Total-Count", Integer.valueOf(itemService.countItems()).toString());
+		headers.add("x-total-count", Integer.valueOf(itemService.countItems()).toString());
+		headers.add("access-control-expose-headers", "x-total-count");
 		
 		List<Item> itemList = itemService.getItemsByPage(page, limit);
 		
